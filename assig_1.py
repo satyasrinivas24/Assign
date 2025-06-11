6e71802f1f8691a324ff08299852b6adc8f2ce91
Satya Srinivas <you@example.com>
2025-06-11 18:51:02 +0530

    assign_1

---
+++ assig_1.py
@@ -0,0 +1,79 @@
+# 1.) full_name and first_name
+# full_name = input("Enter the full_name:")
+# first_name = full_name[0:5]
+# print(first_name)
+
+
+# 2.) input city
+# n=input("Enter the city name:")
+# print(n.upper())
+# print(n.lower())
+# print(n.title())
+
+  # 3.) reversed string
+# n = "Technology"
+# print(n[::-1])
+
+
+ # 4.) combine Data and Science
+# name = "Data" + '-' + "science"
+# print(name)
+ # 5.) product of price 
+# price = float(input("Enter the product price:"))
+# print("product price Rs {:.2f}".format(price))
+
+
+   # 6.) store name & age
+
+# name = 'srinu'
+# age = 21
+# print(f"name is {name} and age is {age} years")
+
+  # 7.) python question
+# sen ='python is awesome'
+# print("is 'python' present?",'python' in sen)
+
+  # 8.) how many characters
+# sen = input("enter the sen:")
+# print("Character count(excluding leading/trailing spaces):",len(sen.strip()))
+
+   #9.) add tax 
+# product_price = 200 
+# tax = 0.05
+# product_price += product_price * tax
+# print(product_price) 
+#10.) Alice name 
+# name = 'Alice'
+# print("is 'i' in name", 'i' in name)
+# print("is 'z' is not in name", 'z' not in name)
+
+
+   # 11)  Average 
+# person_1= int(input("Enter the score of person_1"))
+# person_2= int(input("Enter the score of person_2"))
+# person_3= int(input("Enter the score of person_3"))
+# avg = person_1 + person_2 + person_3 /3
+# print(avg)
+
+
+
+   # 12) height and weight
+# height = int(input("Enter the height in meters "))
+# weight = int(input("enter the weight in kilograms "))
+# BMI = weight / (height ** 2)
+# print(BMI)
+
+
+    # 13) slicing 
+# st = 'i love python'
+# print(st[2:6:])
+ # 14) string method
+# sen = 'This is a bad idea'
+# print(sen.replace("bad", "good"))
+
+
+    # 15)  check equal or not
+# a = 1000
+# b = 1000
+# print("a is b:", a is b)
+ 
